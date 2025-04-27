8d31dd912c18345527026a1c1ef9237d5676acce
RandleH's MacbookAirM1 <randle.helmslay@gmail.com>
2025-04-26 22:48:36 -0700

    Add template

---
+++ template.sh
@@ -0,0 +1,12 @@
+
+
+
+python3 merge.py --dst "<your_destination_path>" \
+--logging debug \
+--no-keep \
+--force \
+"<your_source_file>" \
+"<your_source_file>" \
+"<your_source_file>" \
+"<your_source_file>" \
+"<your_source_file>" \
